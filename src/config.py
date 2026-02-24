330a31eb8a912efea611454e5528c9028bb08901
robin-poelmans <robinpoelmans15@hotmail.com>
2026-02-24 09:49:46 +0100

    Reverting main branch to old version

--- src/config.py
+++ src/config.py
@@ -1,12 +1,9 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Project structure
 PROJECT_ROOT = Path(__file__).parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 SCRIPTS_DIR = PROJECT_ROOT / "scripts"
 
-SOURCE_DB_PATH = Path(os.environ["SOURCE_DB_PATH"])
+SOURCE_DB_PATH = '/media/drives/drive3/robin/plinder/2024-06/v2'
